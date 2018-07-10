b5185f920005fdbd8cfecbb4a6f22dcb8ac68888
SmellyCat <smellycat2014@foxmail.com>
2018-07-10 14:34:30 +0800

    添加 tinydb

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ import time
 import subprocess
 
 import arrow
+from tinydb import TinyDB, Query
 
 from my_yaml import MyYAML
 from my_logger import *
 
 
-debug_logging('/var/logs/error.log')
+debug_logging('logs/error.log')
 logger = logging.getLogger('root')
 
 
@@ -18,27 +19,47 @@ class Dumper(object):
         self.my_ini = self.ini.get_ini()
         self.flag_ini = MyYAML('flag.yaml')
         self.last_time = arrow.get(self.flag_ini.get_ini()['last_time'])
-        #self.last_time = arrow.now('PRC')
+        self.backup_path = self.my_ini['backup_path']
+        self.interval = self.my_ini['interval']
+        self.gc = self.my_ini['gc']
+        db = TinyDB('db.json')
+        self.table = db.table('dump')
 
-    def set_flag(self, time, msg=''):
-        self.last_time = time
-        self.flag_ini.set_ini({'last_time': time.format('YYYY-MM-DDTHH:mm:ssZZ')})
-        logger.info('{0} {1}'.format(time.format('YYYY-MM-DDTHH:mm:ssZZ'), msg))
+    def set_flag(self, date, msg=''):
+        self.last_time = date
+        self.flag_ini.set_ini({'last_time': date.format('YYYY-MM-DDTHH:mm:ssZZ')})
+        logger.info('{0} {1}'.format(date.format('YYYY-MM-DDTHH:mm:ssZZ'), msg))
 
-    def dump(self, ini, date_string):
-        cmd = '/root/tidb-enterprise-tools-latest-linux-amd64/bin/mydumper -h {0} -P {1} -u {2} -p {3} -t 16 -F 64 -B {4} --skip-tz-utc -o /var/dump/{5}/{6}'.format(ini['host'], ini['port'], ini['user'], ini['pwd'], ini['db'], ini['db'], date_string)
+    def dump(self, ini, date):
+        # ±£´æÎ»ÖÃ
+        folder = '{5}/{6}/{7}'.format(self.backup_path, ini['db'], date.format('YYYYMMDDTHHmmss'))
+        # shellÃüÁî
+        cmd = '/root/tidb-enterprise-tools-latest-linux-amd64/bin/mydumper -h {0} -P {1} -u {2} -p {3} -t 16 -F 64 -B {4} --skip-tz-utc -o {5}'.format(ini['host'], ini['port'], ini['user'], ini['pwd'], ini['db'], folder)
         child = subprocess.Popen(cmd, shell=True)
         child.wait()
+        self.table.insert({'cmd': cmd, 'folder': folder, 'created_date': date.format('YYYY-MM-DDTHH:mm:ssZZ')})
         print(cmd)
         logger.info(cmd)
-        logger.info('{0} has been created'.format(date_string))
+
+
+    def clean(self, gc):
+        dumper = Query()
+        items = self.table.all()
+        if items == []:
+            return
+        created_date = arrow.get(items[0]['created_date'])
+        if(time.time() - created_date.timestamp) > gc*24*60*60:
+            self.table.remove(doc_ids=[items[0].doc_id])
+            logger.info('{0} has been remove from TinyDB'.format(items[0]))
+            
     
     def time_check(self):
         now = arrow.now('PRC')
-        if(now.timestamp - self.last_time.timestamp) < float(60*60):
+        if(now.timestamp - self.last_time.timestamp) < self.interval*60*60.0:
             return
-        self.dump(dict(self.my_ini['mysql']), now.format('YYYY-MM-DD_HH:mm:ss'))
+        self.dump(dict(self.my_ini['mysql']), now)
         self.set_flag(now)
+        self.clean(self.gc)
 
     def run(self):
         while 1:
